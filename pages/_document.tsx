9e025b38e27c81f5199b08ece5a00225b47b9187
Evgeny Khristenko <ekhristenko0@gmail.com>
2021-08-27 12:37:49 +0300

    add yandex pixel and editorconfig

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(84593284, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });' }}></script>
+        </body>
+      </Html>
+    );
+  };
+}
+
+export default MyDocument;
