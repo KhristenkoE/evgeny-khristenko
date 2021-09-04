2db3e7d84b93e745202f905206e6003e620d86e2
Evgeny Khristenko <ekhristenko0@gmail.com>
2021-09-04 20:18:04 +0300

    add more pixels

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,13 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(84593284, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });' }}></script>
+          {/* Yandex.Metrica */}
+          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(84593284, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });` }}></script>
+          {/* LinkedIn pixel */}
+          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `_linkedin_partner_id = "3458628"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id);` }}></script>
+          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `(function(l) {if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}var s = document.getElementsByTagName("script")[0];var b = document.createElement("script");b.type = "text/javascript";b.async = true;b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";s.parentNode.insertBefore(b, s);})(window.lintrk);` }}></script>
+          {/* Google Tag Manager */}
+          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5RT5N9K');` }}></script>
         </body>
       </Html>
     );
