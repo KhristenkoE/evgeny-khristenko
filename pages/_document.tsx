9f4f73ccac75829259202c7b0f6ed553d0be22bf
KhristenkoE <ekhristenko0@gmail.com>
2021-12-10 20:30:47 +0500

    add twitter pixel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,6 +40,8 @@ class MyDocument extends Document {
           <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `(function(l) {if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}var s = document.getElementsByTagName("script")[0];var b = document.createElement("script");b.type = "text/javascript";b.async = true;b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";s.parentNode.insertBefore(b, s);})(window.lintrk);` }}></script>
           {/* Google Tag Manager */}
           <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5RT5N9K');` }}></script>
+					{/* Twitter universal website tag code */}
+					<script type="text/javascript" dangerouslySetInnerHTML={{ __html: `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('init','o794x');twq('track','PageView');` }}></script>
         </body>
       </Html>
     );
