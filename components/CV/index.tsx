b1aaa507e91da50e22b81b6bf273f518e7162a44
KhristenkoE <ekhristenko0@gmail.com>
2022-03-27 00:03:43 +0300

    update cv folder

--- components/CV/index.tsx
+++ components/CV/index.tsx
@@ -5,12 +5,12 @@ import styles from './CV.module.sass';
 const CV = () => {
 	return (
 		<Section className={styles.section}>
-			<a className={styles.titleLink} href="../../static/html/cv.html">
+			<a className={styles.titleLink} href="/cv.html">
 				<Title className={styles.title} type={Type.H2}>
 					Curriculum vitae
 				</Title>
 			</a>
-			<iframe src="../../static/html/cv.html" title="CV of Evgeny Khristenko" className={styles.cv} frameBorder="0"></iframe>
+			<iframe src="/cv.html" title="CV of Evgeny Khristenko" className={styles.cv} frameBorder="0"></iframe>
 		</Section>
 	);
 };
